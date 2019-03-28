0d443944a413fb56568aa12e16b0f6629915cec9
Chase A Rayment <chase.rayment@digipen.edu>
2019-03-28 13:31:36 -0700

    Added the Premake script + a sample HelloWorld program.

---
+++ premake/premake5.lua
@@ -0,0 +1,20 @@
+workspace "MAT561EncryptionProject"
+
+  configurations { "Debug", "Release" }
+  
+  location "../build"
+  targetdir "../bin"
+
+  filter { "configurations:Debug" }
+  symbols "On"
+  optimize "Debug"
+  filter {}
+
+  filter { "configurations:Release" }
+  symbols "Off"
+  optimize "Speed"
+  filter {}
+
+  project "HelloWorld"
+  kind "ConsoleApp"
+  files { "../src/HelloWorld.cpp" }
